05924fd325217f723e34928dda1ba1c6790e6be8
YvonneLipLim <164453695+YvonneLipLim@users.noreply.github.com>
2024-10-16 19:54:46 +0800

    Update Treasure_Island_Games.py

--- Treasure_Island_Games/Treasure_Island_Games.py
+++ Treasure_Island_Games/Treasure_Island_Games.py
@@ -32,19 +32,19 @@ while True:
     direction = input("Enter your choice East, West or South? ").lower()
 
     if direction == 'east' or direction.startswith('e'):
-        read_or_watch = input("\nWould you want to read or watch? ").lower()
+        read_or_watch = input("\nWhile you are sailing towards the East, would you want to read a book or watch a movie? ").lower()
         
         if read_or_watch == "read":
-            shake_or_push = input("Would you want to shake or push? ").lower()
+            shake_or_push = input("\nOn the book shelf, you found a small box, would you want to shake the box or push the button to open the box? ").lower()
             
             if shake_or_push == "shake":
-                swim_or_wait = input("\nWould you want to swim or wait? ").lower()
+                swim_or_wait = input("\nAfter some time, you found an island. In the middle of the island, you come to a lake, would you want to swim across or wait for a boat? ").lower()
                 
                 if swim_or_wait == "swim":
-                    fold_cut_press = input("\nWould you want to fold, cut or press? ").lower()
+                    fold_cut_press = input("\nYou've managed to cross the lake but the sky is getting dark. You need to build a tent to rest, would you want to fold your shirt, cut the fruits or press the button? ").lower()
                     
                     if fold_cut_press == "press":
-                        play_eat = input("\nWould you want to play or eat? ").lower()
+                        play_eat = input("\nAt the bottom of the tent, you found a key but your tummy is empty. Would you want to play some music or eat some food? ").lower()
                         
                         if play_eat == "eat":
                             print("Runs out of food. Good luck!")
@@ -67,13 +67,13 @@ while True:
         break
     
     elif direction == 'south' or direction.startswith('s'):
-        swim_or_wait = input("\nWould you want to swim or wait? ").lower()
+        swim_or_wait = input("\nWhile you are sailing towards the South, you found an island. In the middle of the island, you come to a lake, would you want to swim across or wait for a boat? ").lower()
         
         if swim_or_wait == "swim":
-            fold_cut_press = input("\nWould you want to fold, cut or press? ").lower()
+            fold_cut_press = input("\nYou've managed to cross the lake but the sky is getting dark. You need to build a tent to rest, would you want to fold your shirt, cut the fruits or press the button? ").lower()
             
             if fold_cut_press == "press":
-                play_eat = input("\nWould you want to play or eat? ").lower()
+                play_eat = input("\nAt the bottom of the tent, you found a key but your tummy is empty. Would you want to play some music or eat some food? ").lower()
                 
                 if play_eat == "play":
                     print("Congratulations! You found the hidden treasure.")
