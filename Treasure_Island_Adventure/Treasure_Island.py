b3ba3c7ff7e9ddee7ae45d56e9ee00617277f880
YvonneLipLim <164453695+YvonneLipLim@users.noreply.github.com>
2024-10-16 21:26:07 +0800

    Update Treasure_Island.py

--- Treasure_Island_Adventure/Treasure_Island.py
+++ Treasure_Island_Adventure/Treasure_Island.py
@@ -1,6 +1,7 @@
+#Game Introduction
 print("\nWelcome to the Treasure Island.")
 print("Your mission is to find the treasure.")
-print(r'''
+print(r"""
  ____________________________________________________________________
  / \-----     ---------  -----------     -------------- ------    ----\
  \_/__________________________________________________________________/
@@ -25,75 +26,77 @@ print(r'''
  |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
  |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
  / \----- ----- ------------  ------- ----- -------  --------  -------\
- \_/__________________________________________________________________/''')
-print("\nYou are at a crossroad. Which direction would you want to go?")
+ \_/__________________________________________________________________/
+ """)
+print("\nYou are beginning to set sail to hunt for treasure. Which direction would you want to go?")
 
+#Get user input
 while True:
     direction = input("Enter your choice East, West or South? ").lower()
 
     if direction == 'east' or direction.startswith('e'):
-        read_or_watch = input("\nWhile you are sailing towards the East, would you want to read a book or watch a movie? ").lower()
+        read_or_watch = input("\nOn your eastward journey, would you rather delve into a book or be entertained by a movie? Enter read or watch:").lower()
         
         if read_or_watch == "read":
-            shake_or_push = input("\nOn the book shelf, you found a small box, would you want to shake the box or push the button to open the box? ").lower()
+            shake_or_push = input("\nA mysterious box catches your eye on the bookshelf. Would you rather give the box a shake or press the button to unlock its secrets? Enter shake or press:").lower()
             
             if shake_or_push == "shake":
-                swim_or_wait = input("\nAfter some time, you found an island. In the middle of the island, you come to a lake, would you want to swim across or wait for a boat? ").lower()
+                swim_or_wait = input("\nAfter a long voyage, you discover a secluded island. A peaceful lake lies at the island's core, would take the plunge and swim across, or bide your time and wait for a boat? Enter swim or wait:").lower()
                 
                 if swim_or_wait == "swim":
-                    fold_cut_press = input("\nYou've managed to cross the lake but the sky is getting dark. You need to build a tent to rest, would you want to fold your shirt, cut the fruits or press the button? ").lower()
+                    fold_cut_press = input("\nHaving successfully navigated the lake, darkness falls upon you. To prepare for rest, would you prefer to adapt your shirt into a tent, nourish yourself with the fruits, or signal for help by pressing the button? Enter fold or cut or press:").lower()
                     
                     if fold_cut_press == "press":
-                        play_eat = input("\nAt the bottom of the tent, you found a key but your tummy is empty. Would you want to play some music or eat some food? ").lower()
+                        play_eat = input("\nA key lies at the foot of your tent, but your stomach growls. Would you rather soothe your soul with music or satisfy your hunger with food? Enter play or eat:").lower()
                         
                         if play_eat == "eat":
-                            print("Runs out of food. Good luck!")
+                            print("\nRuns out of food. Good luck!")
                             break
                     
                     elif fold_cut_press == "fold":
-                        print("Congratulations! You found the hidden treasure.")
+                        print("\nCongratulations! You found the hidden treasure.")
                         break
                     
                     else:
-                        print("Swept by tsunamis. Hunt is over!")
+                        print("\nSwept by tsunamis. Hunt is over!")
                         break
             
             else:
-                print("You jump I jump. You are in a titanic ship.")
+                print("\nYou jump I jump. You are in a titanic ship.")
                 break
         
         else:
-            print("Sink by whirlpool. Have a spin!")
+            print("\nSink by whirlpool. Have a spin!")
         break
     
     elif direction == 'south' or direction.startswith('s'):
-        swim_or_wait = input("\nWhile you are sailing towards the South, you found an island. In the middle of the island, you come to a lake, would you want to swim across or wait for a boat? ").lower()
+        swim_or_wait = input("\nOn your southward journey, you discover an island. A peaceful lake lies at the island's core, would take the plunge and swim across, or bide your time and wait for a boat? Enter swim or wait:").lower()
         
         if swim_or_wait == "swim":
-            fold_cut_press = input("\nYou've managed to cross the lake but the sky is getting dark. You need to build a tent to rest, would you want to fold your shirt, cut the fruits or press the button? ").lower()
+            fold_cut_press = input("\nHaving successfully navigated the lake, darkness falls upon you. To prepare for rest, would you prefer to adapt your shirt into a tent, nourish yourself with the fruits, or signal for help by pressing the button? Enter fold or cut or press:").lower()
             
             if fold_cut_press == "press":
-                play_eat = input("\nAt the bottom of the tent, you found a key but your tummy is empty. Would you want to play some music or eat some food? ").lower()
+                play_eat = input("\nA key lies at the foot of your tent, but your stomach growls. Would you rather soothe your soul with music or satisfy your hunger with food? Enter play or eat:").lower()
                 
                 if play_eat == "play":
-                    print("Congratulations! You found the hidden treasure.")
+                    print("\nCongratulations! You found the hidden treasure.")
                 else:
-                    print("Runs out of food. Good luck!")
+                    print("\nRuns out of food. Good luck!")
                     break
             
             elif fold_cut_press == "fold":
-                print("Congratulations! You found the hidden treasure.")
+                print("\nCongratulations! You found the hidden treasure.")
                 break
                 
             else:
-                print("Swept by tsunamis. Hunt is over!")
+                print("\nSwept by tsunamis. Hunt is over!")
                 break
         else:
-            print("Attacked by shark. Better luck!")
+            print("\nAttacked by shark. Better luck!")
             break
     
     elif direction == 'west' or direction.startswith('w'):
-        print("You have fell into a hole. Game over!")
+        print("\nYou have fell into a hole. Game over!")
         break
     
     else:
